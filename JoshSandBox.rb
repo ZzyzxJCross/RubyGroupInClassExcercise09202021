aa768f43aa382b04976677ad274aea1fbcec9484
Joshua Cross <70773123+ZzyzxJCross@users.noreply.github.com>
2021-09-24 10:14:07 -0700

    JoshSandBox.rb has can separate lexemes now
    we still need to modify it to be able to explain the tokens though

--- JoshSandBox.rb
+++ JoshSandBox.rb
@@ -1,20 +1,68 @@
-class Token
+#holds token and lexeme together
+class Nonterminal
   def initialize(lexeme, token)
     @lexeme = lexeme
     @token = token
   end
 end
 
+#handles the lexical analysis
 class Lexer
   def initialize
     @inFile = File.read(ARGV[0])
-    print(@inFile)
+    print("The text of the input file is: #@inFile \n")
     @scanIndex = 0
+    @tokenArray = Array.new
   end
 
+  #works through the file and updates the array of token objects
   def fineScanner()
-    workingChar = @inFile[scanIndex]
+
+    #TODO: FIND A WAY TO MAKE IS SO THAT IT USES EOF INSTEAD OF "q"!
+    while(@inFile[@scanIndex] != "q")
+
+      workingChar = @inFile[@scanIndex]
+      workingStr = Array.new
+      workingStr.push(workingChar)
+      indexAugment = 1
+      indexJump = 1
+
+      #handles int constants 
+      #learned how to use regex from https://www.rubyguides.com/2015/06/ruby-regex/
+      if(workingChar =~ /[0-9]/)
+        while(@inFile[@scanIndex + indexAugment] =~ /[0-9]/)
+          workingStr.push(@inFile[@scanIndex + indexAugment])
+
+          print("workingStr is " + workingStr[0] + " ")
+          print("indexAugment is " + indexAugment.to_s + " ")
+          print("@inFile[@scanIndex + indexAugment] is " + @inFile[@scanIndex + indexAugment] + "\n")
+
+          indexAugment = indexAugment + 1
+          indexJump = indexJump + 1
+        end
+      end
+
+      #handles ids
+      if(workingChar =~ /[a-z]/i)
+        while(@inFile[@scanIndex + indexAugment] =~ /[a-z]/i)
+          workingStr.push(@inFile[@scanIndex + indexAugment])
+
+          print("workingStr is " + workingStr[0] + " ")
+          print("indexAugment is " + indexAugment.to_s + " ")
+          print("@inFile[@scanIndex + indexAugment] is " + @inFile[@scanIndex + indexAugment] + "\n")
+
+          indexAugment = indexAugment + 1
+          indexJump = indexJump + 1
+        end
+      end
+
+      @scanIndex = @scanIndex + indexJump
+      puts("the character" + workingStr.to_s + "was found")
+      @tokenArray.push(workingStr)
+    end
+    puts(@tokenArray.to_s)
   end
 end
 
 test = Lexer.new
+test.fineScanner()
