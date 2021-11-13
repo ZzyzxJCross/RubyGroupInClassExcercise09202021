8e9ae06e88e6bfb93ac2eb2abe1fecdee261cc6b
taylor birchem <tbirchem@westmont.edu>
2021-11-12 23:57:20 -0800

    Menu and Window(with ruby 2d)

---
+++ window.rb
@@ -0,0 +1,22 @@
+#writen by Taylor Birchem (10/19/2021)
+#last modified by Taylor Birchem (11/11/2021)
+
+require 'ruby2d'
+
+set background:'white'
+set width: 300, height: 200
+
+board = [
+  " "," "," "," ",
+  " "," "," "," ",
+  " "," "," "," ",
+  " "," "," "," ",
+  " "," "," "," ",
+  " "," "," "," ",
+  " "," "," "," ",
+  " "," "," "," ",]
+
+display_board(board)
+
+
+show
